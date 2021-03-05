b0b2b7e529cc533bd2bf22bdb436ee027588972a
talukder-sowrov <sowrov1000@gmail.com>
2021-03-05 13:09:16 -0500

    Added comments to code and printe out outputs.

--- Lab 2/lab2.m
+++ Lab 2/lab2.m
@@ -1,27 +1,42 @@
 %Output 2 Code follows
+%#ok<*NOPTS>
 
+%Magnetic Levitation Constants
 M = 0.1;
 La = 0.05;
 Ra = 3;
 g = 9.81;
 km = 0.1;
 
+%Equilibrium Value
 ybar = 0.1;
 
-
+%Xbar and Ubar equilibrium values.
 xbar = [ybar;0;(ybar)*sqrt(g*M/km)];
 ubar = ybar*Ra*sqrt(g*M/km);
 
+%Numerical linearization matrices from Simulink
 [A,B,C,D] = linmod('lab2_1', xbar,ubar);
 
+%Theoretical numerical linearization matrices
 A1 = [0 1 0; 2*km/M*((ybar*sqrt(g*M/km))^2)/(ybar^3) 0 -2*km/M*(ybar*sqrt(g*M/km))/ybar^2; 0 0 -Ra/La];
 B1 = [0; 0; 1/La];
 C1 = [1 0 0];
 D1 = 0;
 
+A
+B
+A1
+B1
+
+%Error between theoretical and Simulink matrices
 errorA = norm(A-A1);
 errorB = norm(B-B1);
 
+errorA
+errorB
+
+%State space model and transfer function of Magnetic Levitation system.
 ssmagBall = ss(A1,B1,C1,D1);
 G_magBall = tf(ssmagBall);
 
@@ -32,7 +47,12 @@ zpk_magBall = zpk(ssmagBall);
 [zeros, poles, gain] = zpkdata(G_magBall);
 poles = cell2mat(poles);
 
+poles
+
+%Eigenvalues for A matrix of linearized Magnetic Levitation system.
 eigA1 = eig(A1);
+
+eigA1
 
 
 %Output 3 Code follows
